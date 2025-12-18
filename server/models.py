cf133184642db6ec8b948ea36c5c399cb2725ce7
Blake Godsey <somethingclever182@gmail.com>
2025-12-18 15:37:09 -0600

    Exercise model with validation

--- server/models.py
+++ server/models.py
@@ -1,3 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
+
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    equipment_needed = db.Column(db.Boolean, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "category": self.category,
+            "equipment_needed": self.equipment_needed,
+        }
+
+    @validates("name")
+    def validate_name(self, key, value):
+        if not value or not value.strip():
+            raise ValueError("You must provide an exercise name.")
+        return value.strip()
+
+    @validates("category")
+    def validate_category(self, key, value):
+        if not value or not value.strip():
+            raise ValueError("You must provide an exercise category.")
+        return value.strip()
